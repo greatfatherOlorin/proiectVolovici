753d7786c990df7536aee1fc68105663ef682485
greatfatherOlorin <133770792+greatfatherOlorin@users.noreply.github.com>
2023-05-18 03:36:49 +0300

    movement if piece is selected need to work more

--- proiectVolovici/Form1.cs
+++ proiectVolovici/Form1.cs
@@ -16,22 +16,69 @@ namespace proiectVolovici
 
         ChessBoard board = new ChessBoard();
         Piece piece = new Piece();
- 
+        Boolean isSelected = false;
+  
         public Form1()
         { 
             InitializeComponent();
-            boardPanel.Paint += new PaintEventHandler(boardPanel_Paint);
-               
-
+            
+            boardPanel.Paint+=new PaintEventHandler(boardPanel_Paint);
+            boardPanel.MouseDown += new MouseEventHandler(boardPanel_MouseDown);
+           
         }
 
         private void boardPanel_Paint(object sender, PaintEventArgs e)
         {
-            
+
             board.drawBoard(e.Graphics);
-            piece.drawPieces(e.Graphics);
+
+            board.drawPiece(e.Graphics);
+            board.movePiece(e.Graphics);
 
         }
+
+      
+
+        private void boardPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            int squareSize = 40;
+            int newx = e.X / squareSize;
+            int newy = e.Y / squareSize;
+           
+            if (e.Button == MouseButtons.Left)
+            {
+                int newXPos = newx * squareSize;
+                int newYPos = newy * squareSize;
+
+                if (isSelected)
+                {
+                    board.setWidth(newXPos);
+                    board.setHeight(newYPos);
+                    isSelected = false;
+                    boardPanel.Invalidate();
+                }
+                else
+                {
+                   
+                    if (board.getWidth() == newXPos && board.getHeight() == newYPos)
+                    {
+                        isSelected = true;
+                    }
+                }
+
+              
+                
+
+                
+            }
+
+
+            
+          
+        }
+
+
+
     }
 }
 
